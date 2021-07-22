a922114e12ba249eb3e372e53c61fdd887072a8d
Liviu Andrei <petrache.andrei1@gmail.com>
2021-07-22 14:06:56 +0300

    help

---
+++ include/style.h
@@ -0,0 +1,6 @@
+#ifndef STYLE_H_
+#define STYLE_H_
+
+void drawTitle();
+
+#endif
---
+++ source/style.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/style.h"
+
+void drawTitle()
+{
+    printf("hello this is amari");
+}
