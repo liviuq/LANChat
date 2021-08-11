36b8b9d50053c0f2200d07a876fc755dc546fc91
Liviu Andrei <petrache.andrei1@gmail.com>
2021-08-11 13:01:57 +0300

    start again

--- source/main.c
+++ source/main.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[]) 
 {
-  printf("hello");
-  return 0;
+	printf("hello");
+	return 0;
+	
 }
--- source/style.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "../include/style.h"
-
-void drawTitle()
-{
-    printf("hello this is amari");
-}
