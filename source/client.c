feebbb625b875d9bd3ae0c019d5e12593ceec592
Liviu Andrei <petrache.andrei1@gmail.com>
2021-08-16 15:44:43 +0300

    Made the server and client run untill CTRL+C

--- source/client.c
+++ source/client.c
@@ -8,8 +8,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-int
-main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
@@ -17,67 +16,58 @@ main(int argc, char* argv[])
 
     char *buffer = malloc(sizeof(char) * 256);
     bzero(buffer, 256);
-    if(argc < 3)
+    if (argc < 3)
     {
-        printf("Usage: %s <hostname> <port>\n",*argv);
+        printf("Usage: %s <hostname> <port>\n", *argv);
         exit(EXIT_FAILURE);
     }
-    else 
+    else
         printf("[CLIENT] Parsed input parameters\n");
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
+    if (sockfd < 0)
     {
         printf("[CLIENT] Error opening socket\n");
         exit(EXIT_FAILURE);
-    } 
-    else 
+    }
+    else
         printf("[CLIENT] Opened socket on port %d\n", portno);
 
     server = gethostbyname(argv[1]);
-    if(server == NULL)
+    if (server == NULL)
     {
         printf("[CLIENT] No such host\n");
         exit(EXIT_FAILURE);
-    } 
-    else 
+    }
+    else
         printf("[CLIENT] Trying to connect to %s\n", server->h_name);
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr_list[0], (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-    if(connect(sockfd,(const struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    if (connect(sockfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("[CLIENT] Error connecting\n");
         exit(EXIT_FAILURE);
-    } 
-    else 
+    }
+    else
         printf("[CLIENT] Connected to socket\n");
 
-    printf("Enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-    n = write(sockfd, buffer, strlen(buffer));
-    if(n < 0)
+    while (1)
     {
-        printf("[CLIENT] Error writing to socket\n");
-        exit(EXIT_FAILURE);
-    } 
-    else 
-        printf("[CLIENT] Written %d bytes\n", n);
-
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-    if(n < 0)
-    {
-        printf("[CLIENT] Error reading from socket\n");
-        exit(EXIT_FAILURE);
-    } 
-    else 
-        printf("[CLIENT] Read %d bytes\n", n);
-
-    printf("%s\n", buffer);
+        printf("Enter the message: ");
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+        {
+            printf("[CLIENT] Error writing to socket\n");
+            exit(EXIT_FAILURE);
+        }
+        else
+            printf("[CLIENT] Written %d bytes\n", n);
+    }
 
     return 0;
 }
--- source/server.c
+++ source/server.c
@@ -6,8 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int
-main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int sockfd, newsockfd, portno, clientlen, n;
     char *buffer = malloc(sizeof(char) * 256);
@@ -16,19 +15,19 @@ main(int argc, char* argv[])
 
     if (argc < 2)
     {
-        printf("Usage: %s <port>\n",*argv);
+        printf("Usage: %s <port>\n", *argv);
         exit(EXIT_FAILURE);
     }
-    else 
+    else
         printf("[SERVER] Parsed input parameters\n");
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0)
+    if (sockfd < 0)
     {
         printf("[SERVER] Error opening socket\n");
         exit(EXIT_FAILURE);
-    } 
-    else 
+    }
+    else
         printf("[SERVER] Opened socket\n");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -37,43 +36,45 @@ main(int argc, char* argv[])
     serv_addr.sin_port = htons(portno);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if( bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("[SERVER] Error on binding\n");
         exit(EXIT_FAILURE);
     }
-    else 
+    else
         printf("[SERVER] Binding successfull\n");
 
-    if(listen(sockfd, 5) < 0)
+    if (listen(sockfd, 5) < 0)
     {
         printf("[SERVER] Error on listening\n");
         exit(EXIT_FAILURE);
     }
-    else 
+    else
         printf("[SERVER] Listening on port %d\n", portno);
 
     clientlen = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clientlen);
-    if(newsockfd <0)
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *restrict)&clientlen);
+    if (newsockfd < 0)
     {
         printf("[SERVER] Error on accepting\n");
         exit(EXIT_FAILURE);
     }
-    else 
+    else
         printf("[SERVER] Accepted incoming connection\n");
 
-    bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255);
-    if(n < 0)
+    while (1)
     {
-        printf("[SERVER] Error on reading\n");
-        exit(EXIT_FAILURE);
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+        if (n < 0)
+        {
+            printf("[SERVER] Error on reading\n");
+            exit(EXIT_FAILURE);
+        }
+        else
+            printf("[SERVER] Read %d bytes\n", n);
+
+        printf("The message is: %s\n", buffer);
     }
-    else
-        printf("[SERVER] Read %d bytes\n", n);
-
-    printf("The message is: %s\n", buffer);
-
     return 0;
 }
